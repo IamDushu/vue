c203565c2b1fdd94384d7cf4765435fc775a5151
IamDushu <hey.dushyanth@gmail.com>
2024-07-26 13:58:18 +0530

    Fall through attributes

--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Button from '@/components/Button.vue'
+
 defineProps({
   title: String,
   when: String,
@@ -14,10 +16,7 @@ defineEmits(['register'])
     <p class="p-4 border-b border-gray-200">{{ when }}</p>
     <p class="p-4 text-xs font-medium">{{ description }}</p>
     <section class="flex justify-end p-4">
-        <button class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
-                @click="$emit('register')">
-          Register
-        </button>
+        <Button label="Register" @click="$emit('register')" />
     </section>
   </div>
 </template>
