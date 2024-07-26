6a6834734a08d6aa47420d5814570682bca7e683
IamDushu <hey.dushyanth@gmail.com>
2024-07-26 12:58:01 +0530

    Creating Components and passing props

---
+++ event-booking/src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String
+})
+</script>
+
+<template>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4 text-xs font-medium">{{ description }}</p>
+  </div>
+</template>
