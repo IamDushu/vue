5b600cfdc5ccb42a8059df2e8d55119eeca709c9
IamDushu <hey.dushyanth@gmail.com>
2024-07-27 00:20:46 +0530

    Error Handling and Error Recovery

--- event-booking/src/components/EventList.vue
+++ event-booking/src/components/EventList.vue
@@ -2,17 +2,23 @@
 import { onMounted, ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import LoadingEventCard from '@/components/LoadingEventCard.vue'
+import SectionCard from '@/components/SectionCard.vue'
+import RoundButton from '@/components/RoundButton.vue'
 
 defineEmits(['register'])
 
 const events = ref([])
 const loading = ref(false)
+const error = ref(null)
 
 const fetchEvents = async () => {
- loading.value = true
+  loading.value = true
+  error.value = null;
   try {
     const response = await fetch('http://localhost:3001/events')
     events.value = await response.json()
+  } catch (e) {
+    error.value = e
   } finally {
    loading.value = false
   }
@@ -26,23 +32,35 @@ onMounted(() => {
 <template>
     <div class="space-y-8 lg:w-2/3">
       <h2 class="text-2xl font-medium">All Events</h2>
-      <section class="grid grid-cols-2 lg:grid-cols-3 gap-8">
-        <template v-if="!loading">
-          <template v-if="events.length">
-            <EventCard v-for="event in events" :key="event.id"
-                       :title="event.title" :when="event.date" :description="event.description"
-                       @register="$emit('register', event)"
-            />
+      <template v-if="error">
+        <SectionCard>
+          <div class="space-y-4 items-center flex flex-col">
+            <div class="text-red-500">
+              Could not load events at the moment. Please try again.
+            </div>
+            <RoundButton @click="fetchEvents">Retry now</RoundButton>
+          </div>
+        </SectionCard>
+      </template>
+      <template v-else>
+        <section class="grid grid-cols-2 lg:grid-cols-3 gap-8">
+          <template v-if="!loading">
+            <template v-if="events.length">
+              <EventCard v-for="event in events" :key="event.id"
+                         :title="event.title" :when="event.date" :description="event.description"
+                         @register="$emit('register', event)"
+              />
+            </template>
+            <template v-else>
+              <div class="col-span-2 text-center text-gray-500 lg:col-span-3">
+                No events yet.
+              </div>
+            </template>
           </template>
           <template v-else>
-            <div class="col-span-2 text-center text-gray-500 lg:col-span-3">
-              No events yet.
-            </div>
+            <LoadingEventCard v-for="i in 4" :key="i" />
           </template>
-        </template>
-        <template v-else>
-          <LoadingEventCard v-for="i in 4" :key="i" />
-        </template>
-      </section>
+        </section>
+      </template>
     </div>
 </template>
